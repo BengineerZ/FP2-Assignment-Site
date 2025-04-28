bd9bd665d442e29849b72c1e33d8ae22427bc65c
Ben <bzando@mit.edu>
2025-04-28 09:22:35 -0400

    Updated location of dataset

--- fp2-assignment-site/src/components/scales_plot/CorpOwned.js
+++ fp2-assignment-site/src/components/scales_plot/CorpOwned.js
@@ -9,8 +9,8 @@ const CorpOwned = () => {
       <h2>Shifting the Balance of Boston Housing</h2>
       <h4>How have the corporate ownership rates of housing in the Boston area changed over time, and how does this affect rising housing costs?</h4>
       <CorporateOwnershipScales
-        csvUrl={ `${process.env.PUBLIC_URL}/corporate_sales_boston.csv` }
-        geoJsonUrl={ `${process.env.PUBLIC_URL}/mass-municipalities.geojson` }
+        csvUrl={ `${process.env.PUBLIC_URL}/scale_data/corporate_sales_boston.csv` }
+        geoJsonUrl={ `${process.env.PUBLIC_URL}/scale_data/mass-municipalities.geojson` }
         width={1000}
         height={600}
       />
