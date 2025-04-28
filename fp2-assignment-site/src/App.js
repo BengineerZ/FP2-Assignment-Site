82dc7d84e15ac6206386ab3cd812ba6f84bf2a5a
Nitish Dashora <nrdashora@gmail.com>
2025-04-27 23:50:40 -0400

    no dev proc

--- fp2-assignment-site/src/App.js
+++ fp2-assignment-site/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navigation from './components/nav_dots/Navigation';
 import Header from "./components/Header_Component/Header";
 import Flow from "./components/flow_plot/Flow";
-import DevProc from './components/dev_process/DevProc';
 import ScrollLine from './components/line_connector/ScrollLineConnector';
 import BarChart from './components/bar_chart/BarChart';
 // import Burden from './components/burden_plot/Burden';
