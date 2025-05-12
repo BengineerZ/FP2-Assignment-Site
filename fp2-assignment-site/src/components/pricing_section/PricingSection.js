b7e652478762620e44de9671ad8f952fa58583f2
Nitish Dashora <nrdashora@gmail.com>
2025-05-12 10:42:01 -0400

    text changes

--- fp2-assignment-site/src/components/pricing_section/PricingSection.js
+++ fp2-assignment-site/src/components/pricing_section/PricingSection.js
@@ -12,7 +12,7 @@ const PricingSection = () => {
       
       <div className='text-block'>
       <p>
-        The first effect we see from speculative investment is a rise in housing prices at a disproportionate rate relative to the income growth of the average Bostonian. This is especially pronounced for single-family homes, which allow new renters and homeowners to enter the market. An increasing price for these homes, which isn't matched by income growth, will put many families out of the affordable housing market. This is precisely what we see in the historical data shown below. We display the median single-family home income for Boston in blue, along with the median home sale price for a given year, disaggregated by neighborhood. 
+        The first effect we see from speculative investment is a rise in housing prices at a disproportionate rate relative to the income growth of the average Bostonian. This is especially pronounced for single-family homes, which allow new renters and homeowners to enter the market. An increasing price for these homes, which isn't matched by income growth, will put many families out of the affordable housing market. This is precisely what we see in the historical data shown below. We display the median single-family home income for Boston in blue, along with the median home sale price for a given year, disaggregated by neighborhood, in red. 
       </p>
       </div>
 
@@ -27,7 +27,8 @@ const PricingSection = () => {
         As shown over time, housing prices have dipped from recession and surged from pandemic -- and are now at a historical high. This doesn't necessarily 
         imply an affordable housing shortage, but when we look at the plot, we see an exponential growth in the deficit between the average Bostonian's income and the price of housing.
         This is a clear indicator of the affordability crisis in Boston, and it is only getting worse. Especially in neighborhoods like South End, Back Bay, Beacon Hill, and Longwood, 
-        we see a concerning price growth which is so large, the growth in income is nearly fully overshadowed.
+        we see a concerning price growth which is so large, the growth in income is nearly fully overshadowed. Specifically, switching to the income view allows to see a near 2x growth in
+        median income, while the home price in some neighborhoods has tripled.
       </p>
       </div>
 
